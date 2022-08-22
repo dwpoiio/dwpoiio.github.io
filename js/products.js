03e78fb96f6b6ef6d25972f454939c7e622778b3
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-08-21 23:01:51 -0300

    .

--- js/products.js
+++ js/products.js
@@ -1,12 +1,15 @@
+//pagina a ser cargada
 const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"
-//array donde se cargarán los datos recibidos:
 
+//llamado a la pagina y lectura de json
 fetch(URL)
     .then(res => res.json())
     .then(datos => {
 
         let divListaProductos = document.getElementById("products")
+        //recorro un for para repetir los datos de la pagina
         for (let productos of datos.products) {
+            //organizacion del div con la coleccion de productos
             divListaProductos.innerHTML += ` 
             <div class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
