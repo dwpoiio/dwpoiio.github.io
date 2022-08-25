c9e1d19a9dc4147b057327960f3bfe15bc40c50c
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-08-25 20:46:45 -0300

    entrega 2.1 completo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
 });
+
+// Tomo la informaciond e inicio de sesion
+// Y las guardo en variables
+let nombre_usuario = window.localStorage.getItem('nombre');
+let password_usuario = localStorage.getItem('password');
+document.getElementById("nav-usuario").innerHTML=nombre_usuario;
+
+console.log(nombre_usuario);
+console.log(password_usuario);
---
+++ js/login.js
@@ -0,0 +1,30 @@
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+    document.getElementById("alert-danger").classList.remove("noShow");
+}
+function cerrarDiv(){
+    document.getElementById("alert-danger").classList.add("noShow");
+    document.getElementById("alert-danger").classList.remove("show");
+}
+
+// Selecciono input de usuario y de password
+var nombre = document.getElementById('nombre-usuario');
+var pass = document.getElementById('password');
+
+function login() {
+    // Guardo la informacion de inicio de sesion
+    localStorage.setItem('nombre', nombre.value);
+    localStorage.setItem('password', pass.value);
+    // Verificamos si las condiciones se cumplen 
+    if (nombre.value.length > 0 && pass.value.length > 0) {
+        // Redirigimos a la pagina de portada
+        window.location.href = "portada.html";
+        return true;
+    }
+    else {
+        // Si las condiciones no se cumplen mostramos un mensaje
+        showAlertError();
+        return false;
+    }
+};
+
