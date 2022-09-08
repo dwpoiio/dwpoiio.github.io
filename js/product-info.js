9f4bd0e0fb71b5083af8a2b57bb10bb6f868580e
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-09-08 10:39:18 -0300

    desafiate listo

--- js/product-info.js
+++ js/product-info.js
@@ -4,6 +4,7 @@ let divListaComentarios = document.getElementById("products-info-comentarios");
 let divHacerComentario = document.getElementById("hacer-comentario");
 //creo una variable con el catID guardado en la pagina
 let categoria = localStorage.getItem("productID");
+let div = document.createElement('div');
 
 let nombre_usuario = localStorage.getItem('usuario');
 document.getElementById("nav-usuario").innerHTML = nombre_usuario;
@@ -51,42 +52,76 @@ fetch(PRODUCT_INFO_URL + categoria + ".json")
 fetch(PRODUCT_INFO_COMMENTS_URL + categoria + ".json")
     .then(res => res.json())
     .then(datosComent => {
-        console.log(datosComent)
 
-        for (let comentarios of datosComent) {
-            let div = document.createElement('div');
-            div.innerHTML += `<b>${comentarios.user}</b> - ${comentarios.dateTime} - `
-            divListaComentarios.appendChild(div);
-            //agrego las estrellas naranjas
-            for (let i = 0; i < comentarios.score; i++) {
-                let estrella = document.createElement('span');
-                estrella.classList.add("fa");
-                estrella.classList.add("fa-star");
-                estrella.classList.add("checked");
-                div.appendChild(estrella);
-            }
-            //completo las 5 estrellas negras
-            if (comentarios.score < 5) {
-                let repetir = 5 - comentarios.score;
-                for (i = 0; i < repetir; i++) {
-                    let estrella1 = document.createElement('span');
-                    estrella1.classList.add("fa");
-                    estrella1.classList.add("fa-star");
-                    div.appendChild(estrella1);
+        function innerComentarios() {
+
+            div.innerHTML = "";
+
+
+            for (let comentarios of datosComent) {
+
+                div.innerHTML += `<b>${comentarios.user}</b> - ${comentarios.dateTime} - `
+                divListaComentarios.appendChild(div);
+                //agrego las estrellas naranjas
+                for (let i = 0; i < comentarios.score; i++) {
+                    let estrella = document.createElement('span');
+                    estrella.classList.add("fa");
+                    estrella.classList.add("fa-star");
+                    estrella.classList.add("checked");
+                    div.appendChild(estrella);
                 }
-            }
-            div.innerHTML += `<br>${comentarios.description}<br></br>`
+                //completo las 5 estrellas negras
+                if (comentarios.score < 5) {
+                    let repetir = 5 - comentarios.score;
+                    for (i = 0; i < repetir; i++) {
+                        let estrella1 = document.createElement('span');
+                        estrella1.classList.add("fa");
+                        estrella1.classList.add("fa-star");
+                        div.appendChild(estrella1);
+                    }
+                }
+                div.innerHTML += `<br>${comentarios.description}<br></br>`
+            };
         };
+
+        innerComentarios();
+
         divHacerComentario.innerHTML = `
         <h4>Comentar</h4>
         Tu opinion:<br>
-        <textarea name="textComentario" id="" cols="50" rows="3"></textarea><br>
+        <textarea name="textComentario" id="textComentario" cols="50" rows="3"></textarea><br>
         Tu puntuacion:<br>
-        <select name="transporte">
+        <select name="transporte" id="transporte">
         <option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
         </select><br>
-        <input type="submit" class="btn btn-primary">
+        <input type="button" class="btn btn-primary" id="inputComentario" value="Enviar">
         `
+
+        let botonComentario = document.getElementById("inputComentario");
+        function pushear() {
+            let fecha = new Date()
+            let y = fecha.getFullYear()
+            let m = fecha.getMonth()
+            let d = fecha.getDate()
+            let h = fecha.getHours()
+            let min = fecha.getMinutes()
+            let s = fecha.getSeconds()
+            let array = {
+                product: parseInt(categoria),
+                score: parseInt(transporte.value),
+                description: textComentario.value,
+                user: nombre_usuario,
+                dateTime: y + "-" + m + "-" + d + " " + h + ":" + min + ":" + s
+            }
+            datosComent.push(array);
+            divListaComentarios.removeChild(div);
+            innerComentarios();
+        }
+
+        botonComentario.addEventListener("click", pushear);
+
+
+
     });
 
 
