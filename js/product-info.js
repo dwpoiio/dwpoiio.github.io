cc0a6f641ba113b3b0e5e5e52a0721a21b62eb9d
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-09-22 16:06:13 -0300

    entrega 4 desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -41,23 +41,41 @@ fetch(PRODUCT_INFO_URL + categoria + EXT_TYPE)
             </div>
             <div class="row">
                 <b>Imagenes ilustrativas</b>
-                <div class="col">
-                    <img src="${datos.images[0]}" class="img-thumbnail">
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
-                <div class="col">
-                    <img src="${datos.images[1]}" class="img-thumbnail">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="${datos.images[0]}" class="d-block w-100" alt="img[0]">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="${datos.images[1]}" class="d-block w-100" alt="img[1]">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="${datos.images[2]}" class="d-block w-100" alt="img[2]">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="${datos.images[3]}" class="d-block w-100" alt="img[3]">
+                  </div>
                 </div>
-                <div class="col">
-                    <img src="${datos.images[2]}" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <img src="${datos.images[3]}" class="img-thumbnail">
-                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
             </div>
         </div>`
 
         for (recorrer of datos.relatedProducts) {
-            console.log(recorrer)
             divProductosRelacionados.innerHTML += `
             <div class="col-3 img-thumbnail m-3">
             <div onclick="setProductID(${recorrer.id})" class="list-group-item-action cursor-active">
