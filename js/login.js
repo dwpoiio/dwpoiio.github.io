3476ab3bc881199f35c94e35249dec522a12f165
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-10-31 11:41:36 -0300

    .

--- js/login.js
+++ js/login.js
@@ -2,14 +2,5 @@ let nombre = document.getElementById('nombre-usuario'), pass = document.getEleme
 function showAlertError() { divAlert.classList.remove("fade") }; // Muestro el alerta 
 function cerrarDiv() { divAlert.classList.add("fade") }; // Oculto el alerta
 function login() { // Guardo la informacion de inicio de sesion
-    localStorage.setItem('usuario', nombre.value), localStorage.setItem('password', pass.value);
-    if (nombre.value.length > 0 && pass.value.length > 0) { window.location.href = "index.html" } else { showAlertError() };
+    if (nombre.value.length > 0 && pass.value.length > 0) { localStorage.setItem('usuario', nombre.value), localStorage.setItem('password', pass.value), window.location.href = "index.html" } else { showAlertError() };
 };
-
-// function onSignIn(googleUser) {
-//     var profile = googleUser.getBasicProfile();
-//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//     console.log('Name: ' + profile.getName());
-//     console.log('Image URL: ' + profile.getImageUrl());
-//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-//   }
