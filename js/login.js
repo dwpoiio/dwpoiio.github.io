43551b87158456e74009844d7c09e05d7b9707bb
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2022-10-31 11:27:41 -0300

    login google

--- js/login.js
+++ js/login.js
@@ -1,32 +1,15 @@
-// Selecciono input de usuario y de password
-var nombre = document.getElementById('nombre-usuario');
-var pass = document.getElementById('password');
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-    document.getElementById("alert-danger").classList.remove("noShow");
-}
-function cerrarDiv(){
-    document.getElementById("alert-danger").classList.add("noShow");
-    document.getElementById("alert-danger").classList.remove("show");
-}
-// Borro los datos del usuario
-localStorage.removeItem('usuario');
-localStorage.removeItem('password');
-
-function login() {
-    // Guardo la informacion de inicio de sesion
-    localStorage.setItem('usuario', nombre.value);
-    localStorage.setItem('password', pass.value);
-    // Verificamos si las condiciones se cumplen 
-    if (nombre.value.length > 0 && pass.value.length > 0) {
-        // Redirigimos a la pagina de portada
-        window.location.href = "index.html";
-        return true;
-    }
-    else {
-        // Si las condiciones no se cumplen mostramos un mensaje
-        showAlertError();
-        return false;
-    }
+let nombre = document.getElementById('nombre-usuario'), pass = document.getElementById('password'), divAlert = document.getElementById("alert-danger"); // Selecciono input de usuario y de password
+function showAlertError() { divAlert.classList.remove("fade") }; // Muestro el alerta 
+function cerrarDiv() { divAlert.classList.add("fade") }; // Oculto el alerta
+function login() { // Guardo la informacion de inicio de sesion
+    localStorage.setItem('usuario', nombre.value), localStorage.setItem('password', pass.value);
+    if (nombre.value.length > 0 && pass.value.length > 0) { window.location.href = "index.html" } else { showAlertError() };
 };
+
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//   }
